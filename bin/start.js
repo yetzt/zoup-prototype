4941e04bbacab7f11fa66574ba55834f5e044fa8
yetzt <git@yetzt.me>
2022-05-04 14:03:41 +0200

    remove programmatic npm

--- bin/start.js
+++ bin/start.js
@@ -1,19 +1,19 @@
 // run setup.js if config is empty
 
-const npm = require("npm");
 const conf = require("conf");
 const color = require("kleur");
+const exec = require("child_process").exec;
+const pkg = require("../package.json");
 
-npm.load(function(){
-	const start = function(){
-		const config = new conf();
-		if (!config.has('configured')) return npm.commands.run('setup', function(err){
-			if (err) return console.error(color.red("Setup failed.")), process.exit(1);
-			start();
-		});
-		const pm = (config.get('pm') || "node");
-		console.log(color.magenta("> Starting server with `%s`"), color.white(pm));
-		npm.commands.run(["start-"+pm], function(err){ err && console.error(err); });
-	};
-	start();
-});
+const start = function(){
+	const config = new conf();
+	if (!config.has('configured')) return exec(pkg.scripts.setup, function(err){
+		if (err) return console.error(color.red("Setup failed.")), process.exit(1);
+		start();
+	});
+	const pm = (config.get('pm') || "node");
+	console.log(color.magenta("> Starting server with `%s`"), color.white(pm));
+	exec(pkg.scripts["start-"+pm], function(err){ err && console.error(err); });
+};
+
+start();
